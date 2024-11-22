cb27e8de042deaaf4a5ef620f9d1fe29a5c2f848
Tanit Tuntakij <tanit.tkj@gmail.com>
2024-11-23 00:59:17 +0700

    add minor detail

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -4,11 +4,13 @@ import './Navbar.css';
 const Navbar: React.FC = () => {
   return (
   <>
-    <div className='flex'>
-        <img src='Phone.png' alt="Phone Icon" className='size-7'/>
+    <div className='max-w-[1140px] flex p-0 mx-auto'>
+        <img src='Phone.png' alt="Phone Icon" className='size-7 mr-2.5'/>
         <p>+66 2256 4183, +66 2256 4462</p>
-        <img src='Mail.png' alt="Mail Icon" className='size-7'/>
+        <img src='Mail.png' alt="Mail Icon" className='size-7 mx-1'/>
         <p>prmdcu@gmail.com</p>
+        <img src="/Question.png" alt="Question Icon" className='size-7 mx-1'/>
+        <p>Help Desk</p>
     </div>
   </>
   )
